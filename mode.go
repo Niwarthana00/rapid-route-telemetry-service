04be0349709a52b205d6156695bbfe6e2fdfc3c9
Niwarthana00 <sathyanjali00@gmail.com>
2026-06-11 11:49:23 +0530

    #1 Feat: init project structure

---
+++ mode.go
@@ -0,0 +1,10 @@
+module github.com/yourorg/fleet-tracker-service
+
+go 1.22
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.3
+	github.com/gorilla/mux v1.8.1
+	github.com/rs/zerolog v1.32.0
+	github.com/segmentio/kafka-go v0.4.47
+)
